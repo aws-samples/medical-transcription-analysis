9cc875d9692e45512794bf82826d685ae8bf7549
Kaz Baig <kazbaig@amazon.com>
2021-02-25 16:58:09 -0600

    progress

--- app/src/components/AnalysisPane.js
+++ app/src/components/AnalysisPane.js
@@ -1,10 +1,10 @@
-import React, { useMemo, useState, useCallback } from 'react';
+import React, { useMemo } from 'react';
 
 import s from './AnalysisPane.module.css';
 import cs from 'clsx';
 
 import displayNames from '../displayNames';
-import { Collapse, useDisclosure, Select } from '@chakra-ui/react';
+import { Select, VStack, Box, Flex, IconButton } from '@chakra-ui/react';
 
 const CATEGORIES = [
   'PROTECTED_HEALTH_INFORMATION',
@@ -14,66 +14,73 @@ const CATEGORIES = [
   'TEST_TREATMENT_PROCEDURE',
 ];
 
-function ConceptTable({ rows }) {
-  const { isOpen, onToggle } = useDisclosure();
-
-  return (
-    <>
-      <span className={cs(s.toggleLink, isOpen ? s.expanded : s.contracted)} onClick={onToggle}>
-        {rows.length} detected
-      </span>
-      <Collapse in={isOpen}>
-        <dl>
-          {rows.map((c, i) => (
-            <React.Fragment key={i}>
-              <dt>{c.Code}</dt>
-              <dd>{c.Description}</dd>
-            </React.Fragment>
-          ))}
-        </dl>
-      </Collapse>
-    </>
-  );
-}
-
 function ResultRow({ result, onToggleItem, excludedItems }) {
+  const closeIcon = (
+    <IconButton
+      aria-label='Delete'
+      size='sm'
+      icon={<span>x</span>}
+      border='1px solid black'
+      borderRadius='50%'
+      background='grey'
+      color='white'
+    />
+  );
+
   if (!result.ICD10CMConcepts && !result.RxNormConcepts) {
-    return <div>{result.Text}</div>;
+    return (
+      <Flex width='100%' alignItems='center'>
+        <Flex flex='1' mr={2} height='2.5rem' border='1px solid grey' bg='white' px={4} alignItems='center'>
+          {result.Text}
+        </Flex>
+
+        {closeIcon}
+      </Flex>
+    );
   }
+
   const concepts = [...(result.ICD10CMConcepts ? result.ICD10CMConcepts : result.RxNormConcepts)];
   concepts.sort(function (concept1, concept2) {
     return concept1.Score - concept2.Score;
   });
-  console.log(result);
+
   return (
-    <Select bg='white'>
-      {concepts.map((concept) => (
-        <option key={concept.Code} value={concept.Code}>
-          {result.Text} | {concept.Code} | {concept.Description}
-        </option>
-      ))}
-    </Select>
+    <Flex alignItems='center'>
+      <Select
+        mr={2}
+        border='1px solid'
+        borderColor='grey'
+        borderRadius='0'
+        bg='white'
+        _hover={{ borderColor: 'grey', boxShadow: 'none' }}
+      >
+        {concepts.map((concept) => (
+          <option key={concept.Code} value={concept.Code}>
+            {result.Text} | {concept.Code} | {concept.Description}
+          </option>
+        ))}
+      </Select>
+
+      {closeIcon}
+    </Flex>
   );
 }
 
 function ResultTable({ results, category, onToggleItem, excludedItems }) {
   const filteredResults = useMemo(() => results.filter((r) => r.Category === category), [results, category]);
-  const { isOpen, onToggle } = useDisclosure();
 
   return (
-    <div className={s.resultTable}>
-      <h3 className={isOpen ? s.expanded : s.contracted} onClick={onToggle}>
+    <Box className={s.resultTable} mb={4}>
+      <Box as='h1' mb={4} textAlign='left' fontWeight='bold' fontSize='1.2rem'>
         {displayNames[category]}
-      </h3>
+      </Box>
 
-      <Collapse in={isOpen}>
-        <div>
-          {filteredResults.map((r, i) => (
-            <ResultRow result={r} key={r.id} onToggleItem={onToggleItem} excludedItems={excludedItems} />
-          ))}
-        </div>
-      </Collapse>
-    </div>
+      <VStack spacing={2}>
+        {filteredResults.map((r) => (
+          <ResultRow result={r} key={r.id} onToggleItem={onToggleItem} excludedItems={excludedItems} />
+        ))}
+      </VStack>
+    </Box>
   );
 }
 
