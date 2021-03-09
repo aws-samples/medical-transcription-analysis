e92a88853abe095f34988ea7b0aa3cc9375f7f61
Alex Chirayath <alexchir@3c22fb84644d.ant.amazon.com>
2021-03-08 19:16:27 -0800

    Addressing PR comments

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 import cs from 'clsx';
 import s from './Header.module.css';
 import awsmllogo from '../img/logo_awsml_01.svg';
-
+import { Link } from '@chakra-ui/react';
 import {
   STAGE_HOME,
   STAGE_TRANSCRIBING,
@@ -36,24 +36,12 @@ export default function Header({
     <>
       <header className={cs(s.base, s.visible)}>
         <div className={s.left}>
-          {stage !== STAGE_HOME && stage !== STAGE_EXPORT && stage !== STAGE_SEARCH_EXPORT && stage !== STAGE_SEARCH ? (
+          {stage !== STAGE_HOME && stage !== STAGE_EXPORT && stage !== STAGE_SEARCH_EXPORT && stage !== STAGE_SEARCH && (
             <button onClick={onHome}>
               <span />
               Home
             </button>
-          ) : null}
-          {stage === STAGE_SOAP_REVIEW ? (
-            <button onClick={onHideSOAPReview}>
-              <span />
-              Back
-            </button>
-          ) : null}
-          {stage === STAGE_EXPORT ? (
-            <button onClick={onHideExport}>
-              <span />
-              Back
-            </button>
-          ) : null}
+          )}
 
           <a href='https://aws.amazon.com/machine-learning/ '>
             <img className={s.logo} src={awsmllogo} alt='AWS machine learning' />
@@ -63,30 +51,41 @@ export default function Header({
           <h1>Medical Transcription Analysis</h1>
         </div>
         <div className={s.right}>
-          {stage !== STAGE_SEARCH_EXPORT && stage !== STAGE_SEARCH ? (
+          {stage !== STAGE_SEARCH_EXPORT && stage !== STAGE_SEARCH && (
             <button className={s.search} onClick={onSearch}>
               Search
             </button>
-          ) : null}
-          {stage === STAGE_TRANSCRIBED || stage === STAGE_TRANSCRIBING ? (
-            <button disabled={stage === STAGE_TRANSCRIBING} onClick={onShowSOAPReview}>
-              Review Notes
-            </button>
-          ) : null}
-          {stage === STAGE_SOAP_REVIEW ? <button onClick={onShowExport}>Summarize</button> : null}
-          {stage === STAGE_EXPORT ? <button onClick={onReset}>Start over</button> : null}
-          {stage === STAGE_SEARCH ? (
+          )}
+          {stage === STAGE_SEARCH && (
             <button onClick={onHome}>
               <span />
               Home
             </button>
-          ) : null}
-          {stage !== STAGE_SEARCH_EXPORT ? (
-            <button onClick={handleLogout}>
+          )}
+          {stage === STAGE_SOAP_REVIEW && (
+            <button onClick={onHideSOAPReview}>
               <span />
-              Logout
+              Back
             </button>
-          ) : null}
+          )}
+          {stage === STAGE_EXPORT && (
+            <button onClick={onHideExport}>
+              <span />
+              Back
+            </button>
+          )}
+          {(stage === STAGE_TRANSCRIBED || stage === STAGE_TRANSCRIBING) && (
+            <button disabled={stage === STAGE_TRANSCRIBING} onClick={onShowSOAPReview}>
+              Review Notes
+            </button>
+          )}
+          {stage === STAGE_SOAP_REVIEW && <button onClick={onShowExport}>Summarize</button>}
+          {stage === STAGE_EXPORT && <button onClick={onReset}>Start over</button>}
+          {stage !== STAGE_SEARCH_EXPORT && (
+            <Link onClick={handleLogout} m={4}>
+              Logout
+            </Link>
+          )}
         </div>
       </header>
     </>
