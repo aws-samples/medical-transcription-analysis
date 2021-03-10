9c7661f767b4e159d989663e15f074cf6a5cc848
Alex Chirayath <alexchir@3c22fb84644d.ant.amazon.com>
2021-03-09 17:07:51 -0800

    Revert "Propogate custom entities and selected concepts"
    This reverts commit 74c993c43f082e677b6777ac494accc7fd8d68ea.

--- app/src/home.js
+++ app/src/home.js
@@ -178,9 +178,9 @@ export default function Home() {
 
   useEffect(() => {
     if (!showSOAPReview) {
-      setSOAPSummary(generateSOAPSummary([].concat([...comprehendResults, ...comprehendCustomEntities])));
+      setSOAPSummary(generateSOAPSummary([].concat(...comprehendResults)));
     }
-  }, [comprehendResults, comprehendCustomEntities, showSOAPReview]);
+  }, [comprehendResults, showSOAPReview]);
 
   function updateSOAPSummary(e) {
     setSOAPSummary(e.target.value);
@@ -728,7 +728,7 @@ export default function Home() {
 
         <ExportPane
           transcriptChunks={transcripts}
-          resultChunks={[...comprehendCustomEntities, ...comprehendResults]}
+          resultChunks={comprehendResults}
           excludedItems={excludedItems}
           soapSummary={soapSummary}
           visible={stage === STAGE_EXPORT}
