145dfc006b72d29f30e1f249c590e71d62e286bb
Shivani Mehendarge <61472912+ShivaniMehendarge@users.noreply.github.com>
2021-03-12 15:09:39 -0800

    remove aws ML logo from header (#64)
    * remove aws ML logo from header
    
    * move the heading to the left and link it to home page

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 import cs from 'clsx';
 import s from './Header.module.css';
-import awsmllogo from '../img/logo_awsml_01.svg';
 import { Link } from '@chakra-ui/react';
 import {
   STAGE_HOME,
@@ -42,13 +41,9 @@ export default function Header({
               Home
             </button>
           )}
-
-          <a href='https://aws.amazon.com/machine-learning/ '>
-            <img className={s.logo} src={awsmllogo} alt='AWS machine learning' />
-          </a>
-        </div>
-        <div className={s.headings}>
-          <h1>Medical Transcription Analysis</h1>
+          <h1>
+            <Link onClick={onHome}>Medical Transcription Analysis</Link>
+          </h1>
         </div>
         <div className={s.right}>
           {stage !== STAGE_SEARCH_EXPORT && stage !== STAGE_SEARCH && (
